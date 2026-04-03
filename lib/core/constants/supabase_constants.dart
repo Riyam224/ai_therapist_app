c12a42c6a0ba6a9198a2460f3fedd285e04e221d
Riyam <cs50riyam211@gmail.com>
2026-04-03 18:29:57 +0300

    fix some bugs with colors

---
+++ lib/core/constants/supabase_constants.dart
@@ -0,0 +1,6 @@
+class SupabaseConstants {
+  static const String supabaseUrl = 'https://uivzsuiztocihljdocsp.supabase.co';
+
+  static const String supabaseAnonKey =
+      'sb_publishable_9vfG0paWrUxwbWoewzkuXg_J4RQJqoQ'; // paste full key here
+}
